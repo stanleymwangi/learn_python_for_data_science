5d6bb06567879f0e3a16cc3277a80238b1c95bce
Stanley Mwangi <stanthecoolman@gmail.com>
2018-12-30 11:14:24 +0000

    Create script for tweet analysis

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -3,3 +3,21 @@ import config
 import tweepy
 from textblob import TextBlob
 
+# set up twitter authentication
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+
+# create handler for twitter api calls
+api = tweepy.API(auth)
+
+# get tweet data for specific search term
+tweet_data = api.search("life")
+
+
+for tweet in tweet_data:
+
+    # extract text for analayis
+    tweet_blob = TextBlob(tweet.text)
+
+    # display the tweet and it's sentiment
+    print(tweet.text, tweet_blob.sentiment)
